bf3993d67d72035a1fb1984980c60b19cea4113d
Sobhan <s.kevishahi@gmail.com>
2022-11-13 14:43:56 -0500

    Created Cars

---
+++ classes/car.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+EXIT_POSITION = np.array([2,5])
+
+class Car:
+	# Constructor. Pos is an array of position vectors.
+	def __init__(self, pos:list, length:int, orientation:str, symbol:str, fuel=100):
+		self.positions = pos
+		self.length = length
+		self.orientation = orientation
+		self.symbol = symbol
+		self.fuel = fuel
+
+	# Moves the car moveDistance blocks based on orientation.
+	# If oriented horizontally, then positive is to the right.
+	# If oriented vertically, then positive is to the left.
+	def move(self, moveDistance:int):
+		if self.orientation == "x":
+			for pos in self.positions:
+				pos += np.array([0,moveDistance])
+		else:
+			for pos in self.positions:
+				pos += np.array([moveDistance,0])
+
+	def isAtExit(self):
+		return EXIT_POSITION in self.positions
