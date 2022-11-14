e935d71c4d5a1ea2e02e71239da808de9dab096d
Sobhan <s.kevishahi@gmail.com>
2022-11-14 13:21:38 -0500

    Moved Appropriate Functions To Car

--- car.py
+++ car.py
@@ -11,6 +11,9 @@ class Car:
 		self.symbol = symbol
 		self.fuel = fuel
 
+	def __str__(self):
+		return f"{self.symbol} {self.fuel}"
+
 	# Moves the car moveDistance blocks based on orientation.
 	# If oriented horizontally, then positive is to the right.
 	# If oriented vertically, then positive is to the left.
@@ -36,7 +39,63 @@ class Car:
 
 	def canUseFuel(self, fuelToUse:int):
 		return self.fuel - fuelToUse >= 0
-		
 
 	def isAtExit(self):
 		return EXIT_POSITION in self.positions
+
+def findCarSymbols(string:str):
+	symbols = []
+	characters = set(string)
+	# Create a car for each symbol.
+	for character in characters:
+		if character == ".":
+			continue
+		symbols.append(character)
+	return symbols
+
+def findCarPositions(string:str, symbols:list):
+	allPositions = []
+	for symbol in symbols:
+		symbolPositions = []
+		startIndex = 0
+		index = 0
+		while index != -1:
+			index = string.find(symbol, startIndex)
+			if index != -1:
+				symbolPositions.append((index // 6, index % 6))
+			startIndex = index + 1
+		allPositions.append(symbolPositions)
+	return allPositions
+
+def findCarLengths1(string:str, symbols:list):
+	lengths = []
+	for symbol in symbols:
+		lengths.append(string.count(symbol))
+	return lengths
+
+def findCarLengths2(positions):
+	lengths = []
+	for position in positions:
+		lengths.append(len(position))
+	return lengths
+
+def findCarOrientations(positions):
+	orientations = []
+	for position in positions:
+		if position[0][0] == position[1][0]: # The Ys match.
+			orientations.append("x")
+		else:
+			orientations.append("y")
+	return orientations
+
+def generateCarList(string:str):
+	carSymbols = findCarSymbols(string)
+	carPositions = findCarPositions(string, carSymbols)
+	carOrientations = findCarOrientations(carPositions)
+	carLengths = findCarLengths2(carPositions)
+
+	cars = list()
+	for i in range(len(carSymbols)):
+		cars.append(Car(carPositions[i], carLengths[i], carOrientations[i], carSymbols[i]))
+
+	return cars
