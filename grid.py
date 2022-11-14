0c0e6a6b029fd1ec6d316a2376bf2996d615512b
Sobhan <s.kevishahi@gmail.com>
2022-11-14 13:21:50 -0500

    Added Grid Update

--- grid.py
+++ grid.py
@@ -1,3 +1,5 @@
+from car import *
+
 class Grid:
 	def __init__(self, string:str):
 		self.grid = stringToGrid(string)
@@ -7,6 +9,18 @@ class Grid:
 		for row in self.grid:
 			message += "  ".join(row) + "\n"
 		return message
+
+	def updateGrid(self, car:Car):
+		for i in self.grid:
+			for j in self.grid:
+				if self.grid[i][j] == car.symbol:
+					self.grid[i][j] = "."
+
+		if car.isAtExit():
+			return
+
+		for position in car.positions:
+			self.grid[position[0]][position[1]] = car.symbol
 	
 def stringToGrid(string:str):
 	grid = []
